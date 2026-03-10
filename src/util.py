99cb55e987d5661864c84369f7c3699fd9018dc8
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-03-10 11:02:32 -0400

    reduced certain roots

--- src/util.py
+++ src/util.py
@@ -4,24 +4,32 @@ import subprocess as sp
 import logging as lg
 from test import test_coeff
 
-lg.basicConfig(level=logging.INFO)
+lg.basicConfig(level=lg.INFO)
 log = lg.getLogger(__name__)
 
 def test_and_plot(cf, root_s):
   flag = test_coeff()
 
 def get_input(flag=True):
-  max_, min_ = 20, -21
+  max_, min_ = 10, -11
   elst = list(range(min_,  max_,2)) #even
   olst = list(range(min_+1,max_,2)) #odd
   brkn_per = ran.randint(0,101)
 
-  options = [0               if brkn_per < 30     else 1, 
-            ran.choice(olst) if brkn_per > 50     else ran.choice(elst),
-            ran.choice(elst) if brkn_per % 2 == 0 else ran.choice(olst),]
-  type = [int, float, str]
-  lst = ['f','f','f']
-  
+  def update_lst(flag):
+    lst = ['f', 'f']
+    lst[0] = ran.choice(olst) if flag else ran.choice(elst)
+    lst[1] = ran.choice(olst) if flag else ran.choice(elst)
+    return lst
+
+  sub_lst = update_lst(brkn_per % 2 == 0)
+  options = ['f','f','f']
+  options[0] = 0 if brkn_per < 30 else 1
+  options[1] = -(sum(sub_lst))
+  options[2] = sub_lst[0] * sub_lst[1]
+
+  lst = ['f','f', 'f']
+  type = [int, float, str, bool]
   if not flag:
     for i,o in enumerate(options):
       t = ran.choice(type)
