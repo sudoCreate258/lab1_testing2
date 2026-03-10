4835388591ec052145e46feb62a7a734d31fd0d0
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-03-10 11:25:01 -0400

    draft of test_coeff

---
+++ src/test.py
@@ -0,0 +1,19 @@
+import pytest
+
+def test_single_root(cf):
+  _,b,c = cf
+  assert (b**2 - (4*a*c)) == 0, "discriminent should be 0 for single root"
+
+def test_double_root():
+  _,b,c = cf
+  assert (b**2 - (4*a*c)) > 0, "discriminent should be >0 for double root"
+
+def test_coeff((cf,rts)):
+  if not rts:
+    return False, _
+  else:
+    size = len(rts)
+    if   size == 1:  test_single_root(cf)
+    elif size == 2:  test_double_root(cf)
+    else:            return False
+    return True
