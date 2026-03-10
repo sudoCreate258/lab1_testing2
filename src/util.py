a5a671da1f0b0802045a811a3631ae7a167e20d5
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-03-10 10:08:46 -0400

    draft of get_input

--- src/util.py
+++ src/util.py
@@ -2,10 +2,32 @@
 import random as ran
 
 def get_input(flag=True):
-  if flag:
-    print()
+  max_, min_ = 20, -21
+  elst = list(range(max_,  min_,2))) #even
+  olst = list(range(max_+1,min_,2))) #odd
+  brkn_per = ran.randint(0,101)
+
+  options = [0 if brkn_per < 50 == 0 else 1, ran.choice(elst), ran.choice(olst)]
+  type = [int(), float(), str()]
+  lst = ['f','f','f']
+  
+  if not flag:
+    for i,o in enumerate(options):
+      t = ran.choice(type)
+      lst[i] = t(o)      
   else:
-    print()
+    t = ran.choice(type)
+    for i,o in enumerate(options):
+      lst[i] = t(o)
+      
+  return tuple(lst)
 
 def log_output():
   pass
+
+def print_commits():
+  pass
+
+def test_and_plot():
+  pass
+  
