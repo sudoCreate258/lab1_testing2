69cc63458785b67f2dbcc501fad905f5ad38f105
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-03-11 12:37:04 -0400

    Update util.py

--- src/util.py
+++ src/util.py
@@ -61,8 +61,7 @@ def test_and_plot(cf, root_s):
     plt.axhline(0, color='black', linewidth=1)
     plt.grid(True)
     plt.show()
-    mark = f"{r1}={r2}"
-    fname = f"graphs/lab1_test2_{mark}.png"
+    fname = f"graphs/lab1_test2.png"
     plt.savefig(fname, dpi=300)
     plt.close()
 
