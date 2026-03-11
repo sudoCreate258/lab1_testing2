c9938fb502ce888aa91e830ff8f6efed8ddca122
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-03-11 12:05:02 -0400

    Update log.py

--- src/log.py
+++ src/log.py
@@ -1,4 +1,13 @@
 import logging as lg
 
+lg.basicConfig(
+    filename="lab1_test2.log",
+    level=lg.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    filemode="w"
+)
+
+log = lg.getLogger(__name__)import logging as lg
+
 lg.basicConfig(filename='lab1_test2.log',level=lg.INFO)
 log = lg.getLogger(__name__)
