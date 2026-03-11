f65acad49e9709791755f36cf950af3fab4dd10f
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-03-11 12:12:57 -0400

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,14 +1,12 @@
 from my_logger import log
 
 def test_coeff(cf,rts):
-  if not rts:
-    return False
-  else:
+  if rts:
     size = len(rts)
     a,b,c = cf
     if   size == 1 and (b**2 - (4*a*c)) == 0:
       log.error("discriminent should be 0 for single root")
     elif size == 2 and (b**2 - (4*a*c)) > 0:
       log.error("discriminent should be >0 for double root")
-    else:            
-      return False
+      return True
+  return False
