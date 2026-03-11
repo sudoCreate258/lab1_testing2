16c0bb494821e995ee61729a82d577e0da20a648
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-03-11 12:00:18 -0400

    removed assert for runtime errors

--- src/test.py
+++ src/test.py
@@ -1,9 +1,14 @@
+from util import log
+
 def test_coeff(cf,rts):
   if not rts:
     return False
   else:
     size = len(rts)
     a,b,c = cf
-    if   size == 1:  assert (b**2 - (4*a*c)) == 0, "discriminent should be 0 for single root"
-    elif size == 2:  assert (b**2 - (4*a*c)) > 0, "discriminent should be >0 for double root"
-    else:            return False
+    if   size == 1 and (b**2 - (4*a*c)) == 0:
+      log.info("discriminent should be 0 for single root")
+    elif size == 2 and (b**2 - (4*a*c)) > 0:
+      log.info("discriminent should be >0 for double root")
+    else:            
+      return False
