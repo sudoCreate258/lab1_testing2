f5fec128a0bf5b1b301494608f936c1f56c8c4e7
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-03-11 12:17:08 -0400

    Update util.py

--- src/util.py
+++ src/util.py
@@ -42,17 +42,26 @@ def test_and_plot(cf, root_s):
   flag = test_coeff(cf,root_s)
   if flag:
     r1,r2 = root_s
-    plt.scatter([r1, r2], [0, 0], s=120, color='red', edgecolors='black', zorder=5)
+
+    mid = -b / (2 * a)
     offset = 12 if abs(r1 - r2) > 1.5 else 12
+    x_vals = [i/10 for i in range(int((mid-10)*10), int((mid+10)*10))]
+    y_vals = [a*(xi**2) + b*xi + c for xi in x_vals]
+    
+    plt.plot(x_vals, y_vals)
+    plt.scatter([r1, r2], [0, 0], s=120, color='red', edgecolors='black', zorder=5)
     plt.annotate(f"({r1:.1f}, 0)", (r1, 0), textcoords="offset points", xytext=(0, offset), ha='center')
     plt.annotate(f"({r2:.1f}, 0)", (r2, 0), textcoords="offset points", xytext=(0, -18 if abs(r1-r2) < 1.5 else offset), ha='center')
 
+    x_sym = sp.symbols('x')
+    latex_equation = sp.latex(a*x_sym**2 + b*x_sym + c)
+    
     plt.title(f"$f(x) = {latex_equation}$")
     plt.axhline(0, color='black', linewidth=1)
     plt.grid(True)
     plt.show()
     fname = f"lab1_test2-{str(cf)}.png"
-    plt.savefig(fn, dpi=300)
+    plt.savefig(fname, dpi=300)
 
     log.info(f"graph complete see {fn}")
   else:
