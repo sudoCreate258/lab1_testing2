5f12fd06b42ad0254de500a6186afa908bebe6bf
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-03-11 11:57:07 -0400

    added variables to hold parameters

--- src/test.py
+++ src/test.py
@@ -3,6 +3,7 @@ def test_coeff(cf,rts):
     return False
   else:
     size = len(rts)
+    a,b,c = cf
     if   size == 1:  assert (b**2 - (4*a*c)) == 0, "discriminent should be 0 for single root"
     elif size == 2:  assert (b**2 - (4*a*c)) > 0, "discriminent should be >0 for double root"
     else:            return False
