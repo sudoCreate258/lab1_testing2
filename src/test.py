b2db164f81ec14afd430f35135bae2813522a81a
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-03-11 10:59:52 -0400

    reduced the logic / pytest

--- src/test.py
+++ src/test.py
@@ -1,19 +1,8 @@
-import pytest
-
-def test_single_root(cf):
-  _,b,c = cf
-  assert (b**2 - (4*a*c)) == 0, "discriminent should be 0 for single root"
-
-def test_double_root():
-  _,b,c = cf
-  assert (b**2 - (4*a*c)) > 0, "discriminent should be >0 for double root"
-
 def test_coeff(cf,rts):
   if not rts:
     return False
   else:
     size = len(rts)
-    if   size == 1:  test_single_root(cf)
-    elif size == 2:  test_double_root(cf)
+    if   size == 1:  assert (b**2 - (4*a*c)) == 0, "discriminent should be 0 for single root"
+    elif size == 2:  assert (b**2 - (4*a*c)) > 0, "discriminent should be >0 for double root"
     else:            return False
-    return True
