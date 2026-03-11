f3c89c9a38af734d19e80880d2b684456f0f12e2
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-03-11 17:53:24 -0400

    Update my_logger.py
    revert to manage proper branch usage

--- src/my_logger.py
+++ src/my_logger.py
@@ -1,7 +1,7 @@
 import logging as lg
 
 lg.basicConfig(
-    filename="logs/lab1_test2.log",
+    filename="lab1_test2.log",
     level=lg.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
     filemode="w"
