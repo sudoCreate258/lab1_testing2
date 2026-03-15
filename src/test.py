6b60157bbff4c70dc4968be5710ff2613ed28cf5
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-03-15 15:06:07 -0400

    Update test.py

--- src/test.py
+++ src/test.py
@@ -4,9 +4,12 @@ def test_coeff(cf,rts):
   if rts:
     size = 1 if type(rts) == tuple else 2
     a,b,c = cf
-    if   size == 1 and (b**2 - (4*a*c)) == 0:
+    if   size == 1 and (b**2 - (4*a*c)) != 0:
       log.error(f"discriminent should be 0 for single root abc={cf},roots{rts}")
-    elif size == 2 and (b**2 - (4*a*c)) > 0:
+      return False
+    elif size == 2 and (b**2 - (4*a*c)) < 0:
       log.error(f"discriminent should be >0 for double root abc={cf},roots{rts}")
-    return True
+      return False
+    else:
+      return True
   return False
