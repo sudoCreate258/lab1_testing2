1ff69d0538f5b4ddb4a44ac918b21737c68b9c67
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-03-16 12:45:29 -0400

    removed sample input to create generic input

--- src/main.py
+++ src/main.py
@@ -2,11 +2,8 @@ from util import get_input, test_and_plot
 from my_logger import log
 
 # TODO - (INPUT) receive variable input - step 1
-#my_input = get_input()
+my_input = get_input()
 #xc_input = get_input(False)
-coef, outpt = (1,-5, 6), (2,3)
-#coef, outpt = (1,-6, 5), (1,5)
-#coef, outpt = (1,-2, 1), (1)
 
 
 # TODO - (PROCESS) sanitize variable input - step 2
