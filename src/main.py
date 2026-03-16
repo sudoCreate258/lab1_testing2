a99a2cf12557fc10070a909a1103d23b5bd4b55a
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-03-16 13:46:13 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,9 +2,8 @@ from util import get_input, test_and_plot
 from my_logger import log
 
 # TODO - (INPUT) receive variable input - step 1
-coef = get_input()
+my_input = get_input()
 #xc_input = get_input(False)
-
 
 # TODO - (PROCESS) sanitize variable input - step 2
 
@@ -16,7 +15,7 @@ coef = get_input()
 
 
 # TODO - (OUTPUT) standardize output using logger - step 5
-#log.info(outpt)
+
 
 # TODO - (OUTPUT) test and plot graph - step 6
-test_and_plot(coef, (1,2))
+
