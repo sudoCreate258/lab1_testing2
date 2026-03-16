240f997a9120ad91b60b1aadd274ac0ac929e826
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-03-16 13:26:50 -0400

    removed return t/f for coeff

--- src/test.py
+++ src/test.py
@@ -2,14 +2,30 @@ from my_logger import log
 
 def test_coeff(cf,rts):
   if rts:
-    size = len(rts) if type(rts) == tuple else len(str(rts))
-    a,b,c = cf
-    if   size == 1 and (b**2 - (4*a*c)) != 0:
+    single_rt_flag = type(rts) == tuple and len(rts) == 1
+
+    try:
+      if single_rt_flag:
+        r1,r2 = root_s[0],0
+      else:
+        r1,r2 = root_s[0], root_s[1]
+    except Exception as e:
+      err_log = f"{root_s[0]} {root_s[1]}, {cf} {type(root_s)}, {e}"
+      print(err_log)
+      log.error(err_log)
+      exit()
+    try:
+      a,b,c = cf
+      a/0
+    except Exception as e:
+      err_log = f"{cf}, {e}"
+      print(err_log)
+      log.error(err_log)
+      exit()
+    
+    if single_rt_flag and (b**2 - (4*a*c)) != 0:
       log.error(f"discriminent should be 0 for single root abc={cf},roots{rts}")
-      return False
-    elif size == 2 and (b**2 - (4*a*c)) < 0:
+    elif not single_rt_flag and (b**2 - (4*a*c)) < 0:
       log.error(f"discriminent should be >0 for double root abc={cf},roots{rts}")
-      return False
-    else:
-      return True
-  return False
+
+  return a,b,c
