d45f1b38092931dc389cd49be2e086f0a26ad5d9
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-03-16 13:29:02 -0400

    Update util.py

--- src/util.py
+++ src/util.py
@@ -39,33 +39,34 @@ def get_input(flag=True):
   return tuple(lst)
 
 def test_and_plot(cf, root_s):
-  a,b,c = test_coeff(cf,root_s)
+  cf, flag = test_coeff(cf,root_s)
 
-  mid = -b / (2 * a)
-  offset = 12 if single_rt_flag else (12 if abs(r1 - r2) > 1.5 else 12) 
-  x_vals = [i/10 for i in range(int((mid-10)*10), int((mid+10)*10))]
-  y_vals = [a*(xi**2) + b*xi + c for xi in x_vals]
+  if flag:
+    mid = -b / (2 * a)
+    offset = 12 if single_rt_flag else (12 if abs(r1 - r2) > 1.5 else 12) 
+    x_vals = [i/10 for i in range(int((mid-10)*10), int((mid+10)*10))]
+    y_vals = [a*(xi**2) + b*xi + c for xi in x_vals]
+    
+    plt.plot(x_vals, y_vals)
+    
+    plt.scatter([r1], [0], s=120, color='red', edgecolors='black', zorder=5)
+    plt.annotate(f"({r1:.1f}, 0)", (r1, 0), textcoords="offset points", xytext=(0, offset), ha='center')
+    if not single_rt_flag:
+      plt.scatter([r1, r2], [0, 0], s=120, color='red', edgecolors='black', zorder=5)
+      plt.annotate(f"({r2:.1f}, 0)", (r2, 0), textcoords="offset points", xytext=(0, -18 if abs(r1-r2) < 1.5 else offset), ha='center')
   
-  plt.plot(x_vals, y_vals)
+    x_sym = sp.symbols('x')
+    latex_equation = sp.latex(a*x_sym**2 + b*x_sym + c)
+    
+    plt.title(f"$f(x) = {latex_equation}$")
+    plt.axhline(0, color='black', linewidth=1)
+    plt.grid(True)
+    plt.show()
+    fname = f"graphs/lab1_test2.png"
+    plt.savefig(fname, dpi=300)
+    plt.close()
   
-  plt.scatter([r1], [0], s=120, color='red', edgecolors='black', zorder=5)
-  plt.annotate(f"({r1:.1f}, 0)", (r1, 0), textcoords="offset points", xytext=(0, offset), ha='center')
-  if not single_rt_flag:
-    plt.scatter([r1, r2], [0, 0], s=120, color='red', edgecolors='black', zorder=5)
-    plt.annotate(f"({r2:.1f}, 0)", (r2, 0), textcoords="offset points", xytext=(0, -18 if abs(r1-r2) < 1.5 else offset), ha='center')
-
-  x_sym = sp.symbols('x')
-  latex_equation = sp.latex(a*x_sym**2 + b*x_sym + c)
-  
-  plt.title(f"$f(x) = {latex_equation}$")
-  plt.axhline(0, color='black', linewidth=1)
-  plt.grid(True)
-  plt.show()
-  fname = f"graphs/lab1_test2.png"
-  plt.savefig(fname, dpi=300)
-  plt.close()
-
-  log.info(f"graph complete for a,b,c={cf} and root(s)={root_s} in {fname}")
+    log.info(f"graph complete for a,b,c={cf} and root(s)={root_s} in {fname}")
 else:
   log.warn(f"cannot graph {cf} produces {root_s}")
 
